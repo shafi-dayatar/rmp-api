5aa10a3c21ea365f55a85b8c773374b737ac4393
Yehya Awad <yehyaawad@hotmail.com>
2016-03-25 20:42:00 +0000

    Module setup

---
+++ src/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+(function() {
+  var root = this;
+  var prevRmp = root.rmp;
+  
+  var rmp = function() {
+    var pub = {};
+    /* Get request (Just pretend..) to rmp api */
+    pub.get = function (data) {
+      
+    };
+  };
+
+  rmp.noConflict = function() {
+    root.rmp = prevRmp;
+    return rmp;
+  };
+  
+  // EXPORT
+  if( typeof exports !== 'undefined' ) {
+    if( typeof module !== 'undefined' && module.exports ) {
+      exports = module.exports = rmp;
+    }
+    exports.rmp = rmp;
+  } 
+  else {
+    root.rmp = rmp;
+  }
+}).call(this);
