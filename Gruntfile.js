3ed3da9790063828c8730f4a503079beb53938ed
Yehya Awad <yehyaawad@hotmail.com>
2016-03-25 19:37:27 -0400

    Add Grunt uglify config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,30 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     grunt.initConfig({
-        // our Grunt task settings
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            dist: {
+                options: {
+                    mangle: {
+                        except: ['jQuery'],
+                        sourceMap: true
+                    },
+                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                    '<%= grunt.template.today("yyyy-mm-dd") %> */\n' +
+                    '/* Copyright 2016 (C) Yehya Awad */\n'
+                },
+                files: {
+                    'dist/rmp-api.min.js': [
+                        'src/vedor/cors-bypass.js',
+                        'src/main.js'
+                    ]
+                }
+            }
+        }
     });
+    
+    // Load uglify
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task
+    grunt.registerTask('default', ['uglify:dist']);
 };
